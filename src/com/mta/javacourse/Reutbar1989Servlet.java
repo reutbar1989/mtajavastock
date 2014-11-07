06db13cecf342b6a75d94e064a789ceb512a6719
reut1989 <bar.reut89@gmail.com>
2014-11-07 10:05:53 +0200

    Missiom 2- calculation

--- src/com/mta/javacourse/Reutbar1989Servlet.java
+++ src/com/mta/javacourse/Reutbar1989Servlet.java
@@ -5,7 +5,21 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Reutbar1989Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world Reut");
+		resp.setContentType("text/html");
+		
+		int num1;
+		int num2;
+		int num3;
+
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+
+		int result = (num1+num2) * num3; 
+
+		String resultStr = new String ("<h1>Result of ("+num1+"+"+num2+")*"+num3+"="+result+"</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
+
+
