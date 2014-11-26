dc745af0ff372b6986e729be2ef4ff8877ef4036
reut1989 <bar.reut89@gmail.com>
2014-11-26 14:29:00 +0200

    Exercise 4

--- src/com/mta/javacourse/Reutbar1989Servlet.java
+++ src/com/mta/javacourse/Reutbar1989Servlet.java
@@ -8,34 +8,6 @@ public class Reutbar1989Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
 		
-		int radius = 50;
-		double area;
 		
-		area = radius*radius * Math.PI;
-		
-		String line1 = new String ("calculation 1: Area of circle with radius "+radius+" is: "+area+" square-cm");
-		
-		
-		int hypotenuse = 50;
-		double angleB;
-		float opposite;
-		
-		angleB = 30 * (Math.PI/180);
-		opposite = (float) (hypotenuse * Math.sin(angleB));
-		
-		String line2 = new String ("calculation 2: Length of Opposite where angle B is 30 degrees and Hypotenuse length is "+hypotenuse+" cm is: "+opposite+" cm");
-		
-		
-		double base = 20;
-		double exp = 13;
-		long result;
-		
-		result = (long) Math.pow(base, exp);
-		
-		String line3 = new String ("calculation 3: Power of "+base+" with exp of "+exp+" is "+result+" ");
-		
-
-		String resultStr = line1 + "<br>"+ line2 + "<br>" +line3; 
-		resp.getWriter().println(resultStr);
 	}
 }
---
+++ src/com/mta/javacourse/Stock.java
@@ -0,0 +1,46 @@
+package com.mta.javacourse;
+
+public class Stock{	
+	
+	private String symbol;
+	private float ask;
+	private float bid;
+	private java.util.Date date;
+	
+	public String getSymbol(){
+		return symbol;
+	}
+	
+	public void setSymbol(String symbolS){
+		symbol = symbolS;
+	}
+	
+	public float getAsk(){
+		return ask;
+	}
+	
+	public void setAsk(float askA){
+		ask = askA;	
+	}
+	
+	public float getBid(){
+		return bid;
+	}
+	
+	public void setBid(float bidB){
+		bid = bidB;	
+	}
+	public java.util.Date getDate(){
+		return date;
+	}
+	
+	public void setDate(java.util.Date dateD){
+		date = dateD;
+	}
+	
+	public String getHtmlDescription(){
+		String stockHtmlDetailString = "<b> Stock symbol </b>: "+getSymbol()+", <b> Ask </b>: "+getAsk()+", <b> Bid </b>: "+getBid()+", <b> Date </b>:" +date;
+		return stockHtmlDetailString; 			
+	}
+	
+}
